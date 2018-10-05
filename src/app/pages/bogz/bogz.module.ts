24c1d403d6306fa94e614e792c9e53c78e02a837
Alaude30 <kuramamode11@gmail.com>
2018-10-05 16:21:37 +0800

    Add: login and register component

--- src/app/pages/bogz/bogz.module.ts
+++ src/app/pages/bogz/bogz.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BogzRouting } from './bogz.routing';
 
 import { MaterialModule } from '../../shared/material/material.module';
 
-// Component
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './main/pages/login/login.component';
+import { RegisterComponent } from './main/pages/register/register.component';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, BogzRouting],
-  declarations: [MainComponent]
+  declarations: [MainComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, FormsModule, MaterialModule, BogzRouting]
 })
 export class BogzModule {}
--- src/app/pages/bogz/bogz.routing.ts
+++ src/app/pages/bogz/bogz.routing.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './main/pages/login/login.component';
+import { RegisterComponent } from './main/pages/register/register.component';
 
-const routes: Routes = [{ path: '', component: MainComponent }];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
