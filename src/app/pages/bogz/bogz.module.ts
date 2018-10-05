96cc8d518630abb81293cbd9ebc5a9af1db55fda
Alaude30 <kuramamode11@gmail.com>
2018-10-05 20:31:01 +0800

    Changes: add page routes & restructure

--- src/app/pages/bogz/bogz.module.ts
+++ src/app/pages/bogz/bogz.module.ts
@@ -8,9 +8,14 @@ import { MaterialModule } from '../../shared/material/material.module';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './main/pages/login/login.component';
 import { RegisterComponent } from './main/pages/register/register.component';
+import { FooterComponent } from './main/components/footer/footer.component';
+import { NavbarComponent } from './main/components/navbar/navbar.component';
+import { HomePageComponent } from './main/pages/home-page/home-page.component';
+import { AboutPageComponent } from './main/pages/about-page/about-page.component';
+import { ContactPageComponent } from './main/pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [MainComponent, LoginComponent, RegisterComponent],
+  declarations: [MainComponent, LoginComponent, RegisterComponent, FooterComponent, NavbarComponent, HomePageComponent, AboutPageComponent, ContactPageComponent],
   imports: [CommonModule, FormsModule, MaterialModule, BogzRouting]
 })
 export class BogzModule {}
--- src/app/pages/bogz/bogz.routing.ts
+++ src/app/pages/bogz/bogz.routing.ts
@@ -4,12 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './main/pages/login/login.component';
 import { RegisterComponent } from './main/pages/register/register.component';
+import { HomePageComponent } from './main/pages/home-page/home-page.component';
+import { AboutPageComponent } from './main/pages/about-page/about-page.component';
+import { ContactPageComponent } from './main/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', component: HomePageComponent },
+      { path: 'about', component: AboutPageComponent },
+      { path: 'contact', component: ContactPageComponent }
+    ]
+  },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
