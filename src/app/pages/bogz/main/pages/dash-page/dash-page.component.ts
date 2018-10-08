8f6e736aea8e1e8298b5047a13f41b45633b6dd8
Alaude30 <kuramamode11@gmail.com>
2018-10-08 19:47:17 +0800

    Add: add admin page

---
+++ src/app/pages/bogz/main/pages/dash-page/dash-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-page',
+  templateUrl: './dash-page.component.html',
+  styleUrls: ['./dash-page.component.scss']
+})
+export class DashPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
