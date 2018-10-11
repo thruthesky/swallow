cd7f610174a7c144ed5c3531f499cb78b7dba278
Alaude30 <kuramamode11@gmail.com>
2018-10-11 20:36:29 +0800

    Changes: add Firebase modules

--- projects/bogz/src/app/app.component.ts
+++ projects/bogz/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { AuthTestService } from './services/auth-test.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bogz';
+  constructor(public auth: AuthService, public authTest: AuthTestService) {
+    // this.auth.setDomain({ name: 'prod-domain' });
+    this.authTest.run();
+  }
 }
