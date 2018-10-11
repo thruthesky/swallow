c95c82ab5ce3602b8ef7a7727ebb5208ddd903e7
Alaude30 <kuramamode11@gmail.com>
2018-10-11 20:33:50 +0800

    Add: login form

--- projects/bogz/src/app/pages/login/login.component.ts
+++ projects/bogz/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  constructor(private auth: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async onSubmit(form: FormGroup) {
+    await this.auth.login(form.value);
+
+    if (this.auth.redirectUrl) {
+      this.router.navigate([this.auth.redirectUrl]);
+    }
+
+    this.router.navigate(['/']);
   }
-
 }
