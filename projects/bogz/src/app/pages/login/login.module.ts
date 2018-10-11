143e8dca3670a70af8740e453201346fde6c7709
Alaude30 <kuramamode11@gmail.com>
2018-10-11 20:34:34 +0800

    Add: add register form and register module

--- projects/bogz/src/app/pages/login/login.module.ts
+++ projects/bogz/src/app/pages/login/login.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatCardModule, MatInputModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(<Routes>[{ path: '', component: LoginComponent }])],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(<Routes>[{ path: '', component: LoginComponent }]),
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   declarations: [LoginComponent]
 })
 export class LoginModule {}
--- projects/bogz/src/app/pages/register/register.component.ts
+++ projects/bogz/src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, UserData } from '../../services/auth.service';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthTestService } from '../../services/auth-test.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  constructor(private auth: AuthService, private authTest: AuthTestService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async onSubmit(form: FormGroup) {
+    await this.auth.register(form.value);
+
+    if (this.auth.redirectUrl) {
+      this.router.navigate([this.auth.redirectUrl]);
+    }
+
+    this.router.navigate(['/']);
   }
-
 }
