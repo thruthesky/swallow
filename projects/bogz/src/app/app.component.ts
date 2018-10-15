7a2bb09520f00ddda530651829fe17905e3d1ee5
Alaude30 <kuramamode11@gmail.com>
2018-10-15 15:23:39 +0800

    Changes: turn off post test

--- projects/bogz/src/app/app.component.ts
+++ projects/bogz/src/app/app.component.ts
@@ -1,21 +1,17 @@
-import { Component } from "@angular/core";
-import { AuthService } from "./services/auth.service";
-import { AuthTestService } from "./services/auth-test.service";
-import { PostsTestService } from "./services/posts-test.service";
+import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { AuthTestService } from './services/auth-test.service';
+import { PostsTestService } from './services/posts-test.service';
 
 @Component({
-	selector: "app-root",
-	templateUrl: "./app.component.html",
-	styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	constructor(
-		public auth: AuthService,
-		public authTest: AuthTestService,
-		public postTest: PostsTestService
-	) {
-		// this.auth.setDomain({ name: 'prod-domain' });
-		// this.authTest.run();
-		this.postTest.run();
-	}
+  constructor(public auth: AuthService, public authTest: AuthTestService, public postTest: PostsTestService) {
+    // this.auth.setDomain({ name: 'prod-domain' });
+    // this.authTest.run();
+    // this.postTest.run();
+  }
 }
